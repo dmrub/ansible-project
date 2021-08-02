7a0fb8ad8b7fb9c447a6fd3730ed67d471917aaa
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2021-08-02 16:49:44 +0200

    Bugfix: In configure.sh we currently always have to calculate the shell configuration

--- bin/configure.sh
+++ bin/configure.sh
@@ -4,8 +4,10 @@ set -eo pipefail
 
 THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 
+# BUGFIX: disabled this feature because for some configuration operations
+# environment variables defined in the configuration file must be defined.
 # Disable the call of configure.py from inside the init-env.sh script
-CFG_SHELL_CONFIG_ENABLED=false
+# CFG_SHELL_CONFIG_ENABLED=false
 
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
